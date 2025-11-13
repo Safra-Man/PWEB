18534e2727ee23b2d8964c793cb90c2fb4925f17
William Safra <61418031+Safra-Man@users.noreply.github.com>
2025-11-12 22:08:59 -0300

    Delete PWEBNode/Exercicios directory

--- PWEBNode/Exercicios/exercicio3.js
+++
@@ -1,5 +0,0 @@
-let soma = 7;
-for (let i = 2; i <= process.argv.length - 1; i++) {
-  soma = soma + Number(process.argv[i]);
-}
-console.log("soma=" + soma);
--- PWEBNode/Exercicios/exercicio6.js
+++
@@ -1,5 +0,0 @@
-let http = require('http');
-let server = http.createServer(function(req,res){
-    res.end("<html><body>Site da Fatec Sorocaba</html></body>");    
-});
-server.listen(3000);
